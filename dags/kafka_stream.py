6acf83091f5efb8c95ce404d10078b1a7c0c9eb3
theFutureGuy <ashiqgit789@gmail.com>
2024-02-29 17:18:16 +0530

    implified the error handling within the stream_data()

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -6,6 +6,7 @@ import json
 from kafka import KafkaProducer
 import time
 import logging
+import requests
 
 default_args = {
     'owner': 'airscholar',
@@ -13,22 +14,19 @@ default_args = {
 }
 
 def get_data():
-    import requests
-
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
-
     return res
 
 def format_data(res):
     data = {}
     location = res['location']
-    data['id'] = uuid.uuid4()
+    data['id'] = str(uuid.uuid4())  # Convert UUID to string
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
-    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+    data['address'] = f"{location['street']['number']} {location['street']['name']}, " \
                       f"{location['city']}, {location['state']}, {location['country']}"
     data['post_code'] = location['postcode']
     data['email'] = res['email']
@@ -37,25 +35,18 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
-
     return data
 
 def stream_data():
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    
-    
     curr_time = time.time()
-    while True:
-        if time.time() > curr_time + 60: #1 minute
-            break
+    while time.time() < curr_time + 60:  # Use time condition directly in the loop
         try:
             res = get_data()
             res = format_data(res)
-
             producer.send('users_created', json.dumps(res).encode('utf-8'))
         except Exception as e:
-            logging.error(f'An error occured: {e}')
-            continue
+            logging.error(f'An error occurred: {e}')
 
 with DAG('user_automation',
          default_args=default_args,
